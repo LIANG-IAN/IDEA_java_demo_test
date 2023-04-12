de44485cb93862349c11d7618f3c3b3539e42aca
ASUS <ms0565119@hotmail.com>
2023-04-12 12:03:04 +0800

    add balance and account

--- src/main/java/com/example/java_demo_test/entity/Bank.java
+++ src/main/java/com/example/java_demo_test/entity/Bank.java
@@ -1,4 +1,22 @@
 package com.example.java_demo_test.entity;
 
 public class Bank {
+  private int balance;
+  private String account;
+
+  public int getBalance() {
+    return balance;
+  }
+
+  public void setBalance(int balance) {
+    this.balance = balance;
+  }
+
+  public String getAccount() {
+    return account;
+  }
+
+  public void setAccount(String account) {
+    this.account = account;
+  }
 }
